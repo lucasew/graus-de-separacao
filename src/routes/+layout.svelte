d1a8080000515ab28eed8e7ef3f5e6718cb69667
lucasew <lucas59356@gmail.com>
2025-05-16 22:58:42 -0300

    dont collapse navbar
    Signed-off-by: lucasew <lucas59356@gmail.com>

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,11 +3,6 @@
 	import ForkMeBanner from '../components/ForkMeBanner.svelte';
     import 'bootstrap/dist/css/bootstrap.min.css'
     import 'bootstrap-icons/font/bootstrap-icons.min.css'
-
-    let isNavbarOpen = false;
-    function handleNavbarUpdate(event) {
-        isNavbarOpen = event.detail.isOpen;
-    }
 </script>
 
 
@@ -22,8 +17,6 @@
 <Container style="min-height: 100vh">
     <Navbar expand='md'>
         <NavbarBrand href="/">Graus de separação</NavbarBrand>
-        <NavbarToggler on:click={() => {isNavbarOpen = !isNavbarOpen}} />
-        <Collapse isOpen={isNavbarOpen} navbar expand="md" on:update={handleNavbarUpdate}>
             <Nav>
                 <NavItem>
                     <NavLink href="/list">Lista</NavLink>
@@ -32,7 +25,6 @@
                     <NavLink href="/separation_degrees">Cálculo de graus de separação</NavLink>
                 </NavItem>
             </Nav>
-        </Collapse>
     </Navbar>
 
 <slot/>
